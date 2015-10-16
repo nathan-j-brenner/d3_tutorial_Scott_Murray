c3371d2cb896d1fbdb8e0274413977b1a7914f8d
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-10-15 19:49:04 -0700

    axis

--- d3/scatterplot.js
+++ d3/scatterplot.js
@@ -1,21 +1,32 @@
-var dataset = [
-                [5, 20], 
-                [480, 90], 
-                [250, 50], 
-                [100, 33], 
-                [330, 95],
-                [410, 12], 
-                [475, 44], 
-                [25, 67], 
-                [85, 21],
-                [220, 88],
-                [600, 150]
-              ];
+// var dataset = [
+//                 [5, 20], 
+//                 [480, 90], 
+//                 [250, 50], 
+//                 [100, 33], 
+//                 [330, 95],
+//                 [410, 12], 
+//                 [475, 44], 
+//                 [25, 67], 
+//                 [85, 21],
+//                 [220, 88],
+//                 [600, 150]
+//               ];
+              
+//Dynamic, random dataset
+var dataset = [];
+var numDataPoints = 50;
+var xRange = Math.random() * 1000;
+var yRange = Math.random() * 1000;
+for (var i = 0; i < numDataPoints; i++) {
+    var newNumber1 = Math.round(Math.random() * xRange);
+    var newNumber2 = Math.round(Math.random() * yRange);
+    dataset.push([newNumber1, newNumber2]);
+}
               
 var h = 300;
 var w = 500;
 
-var padding = 20;
+var padding = 30;
             
             
 //scales
@@ -31,7 +42,16 @@ var yScale = d3.scale.linear()
 var rScale = d3.scale.linear()
                     .domain([0, d3.max(dataset, function(d) { return d[1]; })])
                     .range([2,5]);
-  
+
+var xAxis = d3.svg.axis()
+                .scale(xScale)
+                .orient('bottom')
+                .ticks('5');
+
+var yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient('left')
+                .ticks('5')
   
 var svg = d3.select('.scatterplot')
             .append('svg')
@@ -63,27 +83,38 @@ svg.selectAll('circle')
     // .attr('r', 5); //set size
     
 //labels
-svg.selectAll('text')
-    .data(dataset)
-    .enter()
-    .append('text')
-    .text(function(d){
-        return d[0] + ", " + d[1];
-    })
-    //without scale
-    // .attr('x', function(d){
-    //     return d[0];
-    // })
-    // .attr('y', function(d){
-    //     return d[1];
-    // })
-    //with scale
-    .attr('x', function(d){
-        return xScale(d[0]);
-    })
-    .attr('y', function(d){
-        return yScale(d[1]);
-    })
-    .attr('font-family', 'sans-serif')
-    .attr('font-size', '11px')
-    .attr('fill', 'red');
+// svg.selectAll('text')
+//     .data(dataset)
+//     .enter()
+//     .append('text')
+//     .text(function(d){
+//         return d[0] + ", " + d[1];
+//     })
+//     //without scale
+//     // .attr('x', function(d){
+//     //     return d[0];
+//     // })
+//     // .attr('y', function(d){
+//     //     return d[1];
+//     // })
+//     //with scale
+//     .attr('x', function(d){
+//         return xScale(d[0]);
+//     })
+//     .attr('y', function(d){
+//         return yScale(d[1]);
+//     })
+//     .attr('font-family', 'sans-serif')
+//     .attr('font-size', '11px')
+//     .attr('fill', 'red');
+
+//x axis
+svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(0, ' + (h - padding) + ')')
+    .call(xAxis);
+//y axis
+svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(' + padding + ', 0)')
+    .call(yAxis);
